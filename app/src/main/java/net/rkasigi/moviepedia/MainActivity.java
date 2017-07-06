9c4d4227069c1d1ebfb51be0070bdabbbd78eb35
Rendi Kasigi <rkasigi@gmail.com>
2017-07-06 23:03:10 +0700

    initial commit

---
+++ app/src/main/java/net/rkasigi/moviepedia/MainActivity.java
@@ -0,0 +1,25 @@
+/**
+ * This project was born to complete one of the terms of graduation from
+ * Udacity Associate Android Developer Fast Track Nanodegree Program
+ *
+ * July 6, 2016
+ */
+package net.rkasigi.moviepedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * MainActivity
+ *
+ * @author Rendi Kasigi
+ * @version 1.0
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
