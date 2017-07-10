7bfe77cc976af9a9b8cc674862423c3a4a5613a3
Rendi Kasigi <rkasigi@gmail.com>
2017-07-10 20:28:32 +0700

    add error info from api

--- app/src/main/java/net/rkasigi/moviepedia/MovieApiService.java
+++ app/src/main/java/net/rkasigi/moviepedia/MovieApiService.java
@@ -20,7 +20,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -58,7 +60,7 @@ public class MovieApiService {
 
     }
 
-    private List<MovieEntity> jsonToMovieEntity(String jsonString) {
+    private List<MovieEntity> jsonToMovieEntity(String jsonString) throws Exception {
 
         List<MovieEntity> movies = null;
         try {
@@ -88,6 +90,12 @@ public class MovieApiService {
                 }
 
 
+            } else if (jsonObject.has("status_code") && jsonObject.has("status_message")) {
+                throw new Exception(jsonObject.getString("status_message"));
+
+            } else {
+                Log.e(TAG, "return json can not identify: " + jsonString);
+                throw new Exception("failed to identify data");
             }
 
         } catch (JSONException e) {
@@ -133,7 +141,13 @@ public class MovieApiService {
     private String getResponseFromHttpUrl(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         try {
-            InputStream in = urlConnection.getInputStream();
+
+            InputStream in;
+            if(urlConnection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                in = urlConnection.getInputStream();
+            } else {
+                in = urlConnection.getErrorStream();
+            }
 
             Scanner scanner = new Scanner(in);
             scanner.useDelimiter("\\A");
@@ -154,6 +168,48 @@ public class MovieApiService {
             networkErrorListener.invoke(networkErrorListener.getContext(), errorMessage);
         }
 
+    }
+
+    private String moviedbError(int responseHttpCode) {
+        Map<Integer, String> errorMap = new HashMap<Integer, String>()
+        {{
+            put(200,	"Success.");
+            put(501,	"Invalid service: this service does not exist.");
+            put(401,	"Authentication failed: You do not have permissions to access the service.");
+            put(405,	"Invalid format: This service doesn't exist in that format.");
+            put(422,	"Invalid parameters: Your request parameters are incorrect.");
+            put(404,	"Invalid id: The pre-requisite id is invalid or not found.");
+            put(401,	"Invalid API key: You must be granted a valid key.");
+            put(403,	"Duplicate entry: The data you tried to submit already exists.");
+            put(503,	"Service offline: This service is temporarily offline, try again later.");
+            put(401,	"Suspended API key: Access to your account has been suspended, contact TMDb.");
+            put(500,	"Internal error: Something went wrong, contact TMDb.");
+            put(201,	"The item/record was updated successfully.");
+            put(200,	"The item/record was deleted successfully.");
+            put(401,	"Authentication failed.");
+            put(500,	"Failed.");
+            put(401,	"Device denied.");
+            put(401,	"Session denied.");
+            put(400,	"Validation failed.");
+            put(406,	"Invalid accept header.");
+            put(422,	"Invalid date range: Should be a range no longer than 14 days.");
+            put(200,	"Entry not found: The item you are trying to edit cannot be found.");
+            put(400,	"Invalid page: Pages start at 1 and max at 1000. They are expected to be an integer.");
+            put(400,	"Invalid date: Format needs to be YYYY-MM-DD.");
+            put(504,	"Your request to the backend server timed out. Try again.");
+            put(429,	"Your request count (#) is over the allowed limit of (40).");
+            put(400,	"You must provide a username and password.");
+            put(400,	"Too many append to response objects: The maximum number of remote calls is 20.");
+            put(400,	"Invalid timezone: Please consult the documentation for a valid timezone.");
+            put(400,	"You must confirm this action: Please provide a confirm=true parameter.");
+            put(401,	"Invalid username and/or password: You did not provide a valid login.");
+            put(401,	"Account disabled: Your account is no longer active. Contact TMDb if this is an error.");
+            put(401,	"Email not verified: Your email address has not been verified.");
+            put(401,	"Invalid request token: The request token is either expired or invalid.");
+            put(401,	"The resource you requested could not be found.");
+        }};
+
+        return errorMap.get(responseHttpCode);
     }
 
     public enum SortBy {
